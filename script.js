9b7ebd270099e7d0e07a48e5150b9eaf1ae4bb0f
Sareena-28 <sowmicasareena2005@gmail.com>
2025-08-07 21:45:41 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Optional: Add alert or interaction
+console.log("Welcome to Sowmica's portfolio!");
+
+document.addEventListener("DOMContentLoaded", () => {
+  alert("Welcome to my Portfolio 🚀");
+});
